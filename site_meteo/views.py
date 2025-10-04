807d78b1ff77dae47a53aed88109f9a010dd3fe0
Doriane ASSOGBA <assogbadoriane3@gmail.com>
2025-10-04 19:30:49 +0100

    evolution dans le front

--- site_meteo/views.py
+++ site_meteo/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+
+from requests import request
 # Create your views here.
-def home(request):  
-    return render(request, 
-                  'pages/index.html', 
-                  {})
+
+from datetime import date
+from meteo_app import geocode_city, get_weekly_precipitation
+
+def home(request):
+    jour = request.GET.get('date')
+    if not jour:
+        jour = date.today().isoformat()  # format "2025-10-04"
+    return render(request, 'pages/index.html', {'date': jour})
+
+
 def dashboard(request):
     return render(request, 
                   'pages/dashboard.html', 
