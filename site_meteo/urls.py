1cde4d44e065f0ae74b8c214c80ebec9e6034d2a
Doriane ASSOGBA <assogbadoriane3@gmail.com>
2025-10-04 11:38:51 +0100

    finalisation du layout

--- site_meteo/urls.py
+++ site_meteo/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('map/', views.map_view, name='map'),
+    path('suggestions/', views.suggestions, name='suggestions'),
+    path('contact/', views.contact, name='contact'),
 ]
